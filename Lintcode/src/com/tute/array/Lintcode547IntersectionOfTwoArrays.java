d9973e11e2c13bcf6531d702d937b5d7de819ae3
tian <tianzengyin123@gmail.com>
2020-07-12 09:07:20 +0800

    增加lintcode547 和 Leetcode349题 求两个数组的交集

--- Lintcode/src/com/tute/array/Lintcode547IntersectionOfTwoArrays.java
+++ Lintcode/src/com/tute/array/Lintcode547IntersectionOfTwoArrays.java
@@ -3,9 +3,7 @@ package com.tute.array;
 import netscape.security.ForbiddenTargetException;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * @Author: 田增印
@@ -27,11 +25,65 @@ import java.util.List;
 public class Lintcode547IntersectionOfTwoArrays {
     @Test
     public void intersectionTest(){
-       int[] nums1 = {1, 2, 2, 1}, nums2 = {2, 2};
-        int[] intersection = new Lintcode547IntersectionOfTwoArrays().intersection(nums1, nums2);
+       //int[] nums1 = {1, 2, 2, 4,5,7,1}, nums2 = {2, 2,5};
+        int[] nums1 = {61,24,20,58,95,53,17,32,45,85,70,20,83,62,35,89,5,95,12,86,58,77,30,64,46,13,5,92,67,40,20,38,31,18,89,85,7,30,67,34,62,35,47,98,3,41,53,26,66,40,54,44,57,46,70,60,4,63,82,42,65,59,17,98,29,72,1,96,82,66,98,6,92,31,43,81,88,60,10,55,66,82,0,79,11,81};
+        int[] nums2 = {5,25,4,39,57,49,93,79,7,8,49,89,2,7,73,88,45,15,34,92,84,38,85,34,16,6,99,0,2,36,68,52,73,50,77,44,61,48};
+        int[] intersection = new Lintcode547IntersectionOfTwoArrays().intersection2(nums1, nums2);
         for (int i : intersection) {
-            System.out.println(i);
+            System.out.print(i+",");
         }
+    }
+    public int[] intersection3(int[] nums1, int[] nums2){
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0, j = 0;
+        int temp[] = new int[nums1.length];
+        int index = 0;
+        while (i<nums1.length&&j<nums2.length){
+            if(nums1[i]==nums2[j] ){
+                if(index==0||temp[index-1]!=nums1[i] ){
+                    temp[index++] = nums1[i];
+                }
+                i++;
+                j++;
+            }else if(nums1[i] < nums2[j] ){
+                i++;
+            }else {
+                j++;
+            }
+        }
+        int[] result = new int[index];
+        System.arraycopy(temp,0 ,result ,0 ,index );
+//        for (int k = 0; k <index ; k++) {
+//            result[k] = temp[k];
+//        }
+        return result;
+    }
+
+    public int[] intersection2(int[] nums1, int[] nums2){
+        HashSet<Integer> nums1Set = new HashSet<>();
+        HashSet<Integer> nums2Set = new HashSet<>();
+        for (int i : nums1) {
+            nums1Set.add(i);
+        }
+        for (int i : nums2) {
+            nums2Set.add(i);
+        }
+        ArrayList<Integer> lists = new ArrayList<>();
+        for (Integer integer : nums1Set) {
+            if(nums2Set.contains(integer) ){
+                lists.add(integer);
+            }
+        }
+        int size = lists.size();
+        int[] result = new int[size];
+        int index = 0;
+        for (Integer integer : lists) {
+            result[index++] = integer;
+        }
+         //int[] arr1 = lists.stream().mapToInt(Integer::valueOf).toArray();
+        return result;
+        //Integer[] integers = lists.toArray(new Integer[lists.size()]);
     }
     public int[] intersection(int[] nums1, int[] nums2) {
         // write your code here
