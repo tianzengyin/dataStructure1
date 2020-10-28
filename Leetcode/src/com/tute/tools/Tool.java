4891e1a9ceb723009041b64134e46aa0c1257787
tian <tianzengyin123@gmail.com>
2020-10-28 15:35:02 +0800

    增加工具类

--- Leetcode/src/com/tute/tools/Tool.java
+++ Leetcode/src/com/tute/tools/Tool.java
@@ -1,5 +1,7 @@
 package com.tute.tools;
 
+import java.util.Random;
+
 /**
  * @Author: 田增印
  * @Date: 2020/10/28
@@ -7,5 +9,22 @@ package com.tute.tools;
  * @Description: com.tute.tools
  * @VERSION: 1.0
  */
+
+
 public class Tool {
+    /**
+     * 生成随机数组
+     * @param n 数组长度
+     * @param randomL 数组最小元素
+     * @param randomR 数组最大元素
+     * @return
+     */
+    public static int[] generateIntArray(int n ,int randomL, int randomR){
+        int[] arr = new int[n];
+        for (int i = 0; i <arr.length ; i++) {
+            int num = new Random().nextInt(randomR-randomL+1)+randomL;
+            arr[i] = num;
+        }
+        return arr;
+    }
 }
