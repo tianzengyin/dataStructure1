7625241d5301999ae38fea8af67cb327a05dcd95
tian <tianzengyin123@gmail.com>
2020-10-28 15:36:54 +0800

    Leetcode69

--- Leetcode/src/com/tute/Leetcode69.java
+++ Leetcode/src/com/tute/Leetcode69.java
@@ -1,5 +1,7 @@
 package com.tute;
 
+import org.junit.Test;
+
 /**
  * @Author: 田增印
  * @Date: 2020/8/23
@@ -8,4 +10,24 @@ package com.tute;
  * @VERSION: 1.0
  */
 public class Leetcode69 {
+    @Test
+    public void  test1(){
+        int i = new Leetcode69().mySqrt1(8);
+        System.out.println(i);
+    }
+    public int mySqrt(int x) {
+        int last_n = x,n = x/2;
+        while (last_n - n > 1e-4){
+            last_n = n;
+            n = (last_n + x/last_n)/2;
+        }
+        return n;//int(n) ** 2 ==x
+    }
+    public int mySqrt1(int x){
+        long n = x;
+        while (n * n > x) {
+            n = (n + x / n) / 2;
+        }
+        return (int) n;
+    }
 }
